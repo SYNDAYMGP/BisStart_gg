14aeff5744698ebe299a8301a2cd684924fd6ce9
Dream DaGger <142337606+SYNDAYMGP@users.noreply.github.com>
2023-12-01 11:47:55 +0300

    Update osobenosti-kod.py

--- osobenosti-kod.py
+++ osobenosti-kod.py
@@ -52,7 +52,7 @@ else:
 
 #--------------ЗАПИСЬ ГЛ И ПОДГЛ В ПЕРЕМЕННЫЕ--------------
 # Открываем файл для чтения
-with open('1.txt', 'r', encoding='utf-8') as file:
+with open('1.txt', 'r', encoding=x) as file:
     # Инициализируем переменные
     current_variable = ""
     ww_vars = []
@@ -61,7 +61,7 @@ with open('1.txt', 'r', encoding='utf-8') as file:
         # Проверяем, содержит ли строка цифру
         if any(char.isdigit() for char in line):
             # Если переменная не пуста, сохраняем ее в список переменных
-            if current_variable:
+            if current_variable.strip():  # Добавлена проверка на пустоту
                 ww_vars.append(current_variable)
             # Создаем новую переменную и добавляем текущую строку с цифрой
             current_variable = line
@@ -69,12 +69,11 @@ with open('1.txt', 'r', encoding='utf-8') as file:
             # Добавляем текущую строку с текстом к текущей переменной
             current_variable += line
     # Добавляем последнюю переменную в список (если она не пуста)
-    if current_variable:
+    if current_variable.strip():  # Добавлена проверка на пустоту
         ww_vars.append(current_variable)
 # Выводим результат
 for i, variable in enumerate(ww_vars, 1):
     print(f"ww_vars[{i-1}]:\n{variable}")
-print(ww_vars[15])
 
 
 
